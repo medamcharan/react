eec99250f834acd9f443c1fa2dec0e53a3fb4ec3
medamcharan <138815544+medamcharan@users.noreply.github.com>
2024-05-24 01:25:43 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,10 +35,9 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        mah dear kundanapu bomma🖤,<br />
-   As I sit here, miles apart from you, my heart aches for your presence. Every moment without you feels like an eternity, and I find myself longing for the day when we can be together again. Your love is my guiding light, and no distance can diminish the depth of my feelings for you. I cherish every memory we've created together and eagerly anticipate the countless moments we have yet to share. Until the day we are reunited, know that you are always in my thoughts and forever in my heart.,<br />
-        With all my love,
-@charan
+        My dear kundanapu bomma🖤🦋,<br />
+   My love, though miles apart, my heart beats for you alone. Every moment without you is a countdown to our reunion. Until then, know you're my everything..,<br />
+        With all my love 💕,@charan
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
